eec2428a47961f4e0fb651f720bb966131b09c16
NintenZone <griz6400@gmail.com>
2018-03-05 11:03:31 -0700

    Electron Rewrite

--- index.js
+++ index.js
@@ -1,119 +1,106 @@
-//Config
-let beta = false;
-let version = 1;
+//config
+let production = true;
+let version = 2;
 
-const RPC = require('discord-rich-presence')('387406899739623426');
-
-console.log('Loading SwitchRPC Beta-1.0...');
-console.log('Loading control panel...');
-
-const request = require('request');
-
-console.log('Loaded control panel successfully.');
-console.log('Loading http module...');
-
-const http = require('http');
-const formidable = require('formidable');
-
-console.log('Loaded http module successfully.');
-
-console.log('Starting up the file system...');
-const fs = require('fs');
-console.log('File system started.');
-
-console.log("Connecting to your Discord client...");
 const rpc = require('discord-rich-presence')('387406899739623426');
-console.log("Connected to your client successfully.");
+const request = require('request');
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
 
-console.log('Just setting some extra things up...');
-const util = require('util');
-console.log('We\'re almost ready to go!');
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
-console.log('Creating the control panel, prepare for liftoff!');
-
-var server = http.createServer(function (req, res) {
-    if (req.method.toLowerCase() === 'get') {
-        displayForm(res);
-    }
-    else if (req.method.toLowerCase() === 'post') {
-        processIndividualFields(req, res);
-    }
-
-});
-
-function displayForm(res) {
-    fs.readFile('./index.html', function (err, data) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-                'Content-Length': data.length
-        });
-        res.write(data);
-        res.end();
-    });
-}
-
-server.listen(1185);
-
+let mainWindow;
+let loadingWindow;
+let updateWindow;
 let wData;
 
-console.log('Fetching latest data from server...');
-request('http://nintenbot.js.org/rpc.json', function(error, res, body) {
-    wData = JSON.parse(body);
-    console.log('Loaded data from server.')
-    if (version < wData.version && beta === false) {
-        console.log("This build is outdated! Please download the latest version from https://github.com/NintenZone/SwtichRPC/releases/");
-        process.exit();
-    }
+if (handleSquirrelEvent(app)) {
+    return;
+}
+
+//Listen For App to be Ready
+app.on('ready', function() {
+    //create window
+    mainWindow = new BrowserWindow({});
+    //Load HTML
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'loading.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    request('http://nintenbot.js.org/rpc.json', function(error, res, body) {
+        wData = JSON.parse(body);
+        if (version < wData.version && production === true) {
+            mainWindow.loadURL(url.format({
+                pathname: path.join(__dirname, 'update.html'),
+                protocol: 'file:',
+                slashes: true
+            }));
+        }
+        else {
+            mainWindow.loadURL(url.format({
+                pathname: path.join(__dirname, 'index.html'),
+                protocol: 'file:',
+                slashes: true
+            }));
+        }
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    //Menu Template
+    const home = Menu.buildFromTemplate(homeTemplate);
+
+    Menu.setApplicationMenu(home);
 
 });
-console.log("Control panel created. Ready to rumble!");
-console.log("Server listening on http://localhost:1185.");
-console.log("Please go to http://localhost:1185 in your browser to continue.");
-
-function processAllFields(req, res) {
-    var form = new formidable.IncomingForm();
-
-    form.parse(req, function (err, fields, files) {
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        });
-        res.write('Nice, getting right on that!');
-    });
-}
 
 let game;
 let desc;
+//Catch Values
+ipcMain.on('game:value', function(e, value) {
+    game = value;
+    checkSet();
+});
+ipcMain.on('desc:value', function(e, value) {
+    desc = value;
+    checkSet();
+});
 
-function processIndividualFields(req, res) {
-    var fields = [];
-    var form = new formidable.IncomingForm();
-    form.on('field', function (field, value) {
-        if (field === "game") {
-            game = value;
-        }
-        if (field === "desc") {
-            desc = value;
-        }
-        setData()
-    });
 
-    form.on('end', function() {
-        res.writeHead(200, {
-            'content-type': 'text/html'
-        });
-        res.write('Nice! Getting right on that.\n' +
-            '<a href="http://localhost:1185/">Click Here To Set Your Status Again</a>');
-        res.end();
-    });
-    form.parse(req);
-}
+//create menu template
+const homeTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Exit',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
 
-function setData() {
-    let gameArr = wData.games;
-    let gameObj = wData.gameData;
+//RPC Stuff
 
+function checkSet() {
     if (!game) return;
     if (!desc) return;
+    findGame()
+}
+
+let pic;
+function findGame() {
+    let gameArr = wData.games;
+    let gameObj = wData.gameData;
 
     if (gameArr.indexOf(game.toLowerCase()) >= 0) {
         let num = gameArr.indexOf(game.toLowerCase());
@@ -121,7 +108,7 @@ function setData() {
         let pic = gameObj[num].pic;
         setPresence(name, pic);
     }
-    else{
+    else {
         let name = game;
         let pic = "switch";
         setPresence(name, pic)
@@ -133,8 +120,73 @@ function setPresence(name, pic) {
         state: desc,
         details: name,
         largeImageKey: pic,
+        largeImageText: name,
         smallImageKey: 'online',
+        smallImageText: 'Online',
         instance: false,
     });
 }
 
+function handleSquirrelEvent(application) {
+    if (process.argv.length === 1) {
+        return false;
+    }
+
+    const ChildProcess = require('child_process');
+    const path = require('path');
+
+    const appFolder = path.resolve(process.execPath, '..');
+    const rootAtomFolder = path.resolve(appFolder, '..');
+    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+    const exeName = path.basename(process.execPath);
+
+    const spawn = function(command, args) {
+        let spawnedProcess, error;
+
+        try {
+            spawnedProcess = ChildProcess.spawn(command, args, {
+                detached: true
+            });
+        } catch (error) {}
+
+        return spawnedProcess;
+    };
+
+    const spawnUpdate = function(args) {
+        return spawn(updateDotExe, args);
+    };
+
+    const squirrelEvent = process.argv[1];
+    switch (squirrelEvent) {
+        case '--squirrel-install':
+        case '--squirrel-updated':
+            // Optionally do things such as:
+            // - Add your .exe to the PATH
+            // - Write to the registry for things like file associations and
+            //   explorer context menus
+
+            // Install desktop and start menu shortcuts
+            spawnUpdate(['--createShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-uninstall':
+            // Undo anything you did in the --squirrel-install and
+            // --squirrel-updated handlers
+
+            // Remove desktop and start menu shortcuts
+            spawnUpdate(['--removeShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-obsolete':
+            // This is called on the outgoing version of your app before
+            // we update to the new version - it's the opposite of
+            // --squirrel-updated
+
+            application.quit();
+            return true;
+    }
+}
